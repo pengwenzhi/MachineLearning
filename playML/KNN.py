ff76e9117c2b4be85f2e115ae7956e4143360a6e
Peng <18130504973@163.com>
2021-05-20 23:57:55 +0800

    5月20号学习

--- playML/KNN.py
+++ playML/KNN.py
@@ -7,15 +7,15 @@ class KNNClassifier:
         assert k >=1,"k must be valid"
         self.k=k
         self._X_train=None
-        self._Y_train=None
-    def fit(self,X_train,Y_train):
-        assert X_train.shape[0] == Y_train.shape[0], \
+        self._y_train=None
+    def fit(self,X_train,y_train):
+        assert X_train.shape[0] == y_train.shape[0], \
             "the size of X_train must equal to the size of Y_train"
         assert self.k<=X_train.shape[0],\
             "the size of X_train must be at least k."
 
         self._X_train=X_train
-        self._Y_train=Y_train
+        self._y_train=y_train
         return self
     def predict(self,X_predict):
         assert self._X_train is not None and self._X_train is not None,\
@@ -29,21 +29,21 @@ class KNNClassifier:
             "the feature number of x must be equal to X_train"
         distances = [sqrt(np.sum((x_train - x) ** 2)) for x_train in self._X_train]
         nearest = np.argsort(distances)
-        topK_Y = [self._Y_train[i] for i in nearest[:self.k]]
-        votes = Counter(topK_Y)
+        topK_y = [self._y_train[i] for i in nearest[:self.k]]
+        votes = Counter(topK_y)
         return votes.most_common(1)[0][0]
     def __repr__(self):
         return "KNN(k=%d)"%self.k
 
 
-def KNN_classify(k,X_train,Y_train,x):
+def KNN_classify(k,X_train,y_train,x):
     assert 1<=k<=X_train.shape[0],"K must be vaild"
-    assert X_train.shape[0]==Y_train.shape[0],\
+    assert X_train.shape[0]==y_train.shape[0],\
         "the size of X_train must equal to the size of Y_train"
     assert X_train.shape[1]==x.shape[0],\
         "the feature number of x must be equal to X_train"
     distances=[sqrt(np.sum((x_train-x)**2)) for x_train in X_train]
     nearest=np.argsort(distances)
-    topK_Y=[Y_train[i] for i in nearest[:k]]
-    votes=Counter(topK_Y)
+    topK_y=[y_train[i] for i in nearest[:k]]
+    votes=Counter(topK_y)
     return votes.most_common(1)[0][0]
--- playML/model_selection.py
+++ playML/model_selection.py
@@ -1,6 +1,6 @@
 import numpy as np
-def train_test_split(X,Y,test_ratio=0.2,seed=None):
-    assert X.shape[0]==Y.shape[0],\
+def train_test_split(X,y,test_ratio=0.2,seed=None):
+    assert X.shape[0]==y.shape[0],\
         "the size of X must be equal to the size of Y"
     assert 0.0<=test_ratio<=1.0,\
         "test_ratio must be valid"
@@ -8,13 +8,15 @@ def train_test_split(X,Y,test_ratio=0.2,seed=None):
     if seed:
         np.random.seed(seed)
 
-    shuffled_indexes=np.random.premutation(len(X))
+    shuffled_indexes=np.random.permutation(len(X))
     test_size=int(len(X)*test_ratio)
     test_indexes=shuffled_indexes[:test_size]
     train_indexes=shuffled_indexes[test_size:]
 
     X_train=X[train_indexes]
-    Y_train=Y[train_indexes]
+    y_train=y[train_indexes]
 
     X_test=X[test_indexes]
-    Y_test=Y[test_indexes]
+    y_test=y[test_indexes]
+
+    return X_train,X_test,y_train,y_test
