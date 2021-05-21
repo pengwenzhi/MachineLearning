6c1caebd72d8762f3bfcf75a45d079fa52b669f5
Peng <18130504973@163.com>
2021-05-22 00:19:54 +0800

    5月21号学习

--- playML/SimpleLinearRegression.py
+++ playML/SimpleLinearRegression.py
@@ -1,83 +1,47 @@
 import numpy as np
+from .metrics import r2_score
 
-class SimpleLinearRegression1:
+
+class SimpleLinearRegression:
+
     def __init__(self):
-        self.a=None
-        self.b=None
+        """初始化Simple Linear Regression模型"""
+        self.a_ = None
+        self.b_ = None
 
-    def fit(self,x_train,y_train):
-        assert x_train.ndim==1,\
-            "Simple Linear Regressior can only solve single feature training data"
-        assert len(x_train)==len(y_train),\
-            "the size of x_train be equal to the size of y_train"
-        x_mean=np.mean(x_train)
-        y_mean=np.mean(y_train)
+    def fit(self, x_train, y_train):
+        """根据训练数据集x_train, y_train训练Simple Linear Regression模型"""
+        assert x_train.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature training data."
+        assert len(x_train) == len(y_train), \
+            "the size of x_train must be equal to the size of y_train"
 
-        # num=0.0
-        # d=0.0
-        #
-        # for x,y in zip(x_train,y_train):
-        #     num+=(x-x_mean)*(y-y_mean)
-        #     d+=(x-x_mean)**2
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
 
-        num=(x_train-x_mean).dot(y_train-y_mean)
-        d=(x_train-x_mean).dot(x_train-x_mean)
+        self.a_ = (x_train - x_mean).dot(y_train - y_mean) / (x_train - x_mean).dot(x_train - x_mean)
+        self.b_ = y_mean - self.a_ * x_mean
 
-        self.a_=num/d
-        self.b_=y_mean-self.a_*x_mean
         return self
 
-    def predict(self,x_predict):
-        assert x_predict.ndim==1,\
-            "Simple Linear Regressor can only solve single feature training data"
-        assert self.a_ is not None and self.b_ is not None,\
+    def predict(self, x_predict):
+        """给定待预测数据集x_predict，返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature training data."
+        assert self.a_ is not None and self.b_ is not None, \
             "must fit before predict!"
+
         return np.array([self._predict(x) for x in x_predict])
 
-    def _predict(self,x_single):
-        return self.a_*x_single+self.b_
+    def _predict(self, x_single):
+        """给定单个待预测数据x，返回x的预测结果值"""
+        return self.a_ * x_single + self.b_
+
+    def score(self, x_test, y_test):
+        """根据测试数据集 x_test 和 y_test 确定当前模型的准确度"""
+
+        y_predict = self.predict(x_test)
+        return r2_score(y_test, y_predict)
 
     def __repr__(self):
-        return "SimpleLinearRegression1()"
-
-
-class SimpleLinearRegression2:
-    def __init__(self):
-        self.a=None
-        self.b=None
-
-    def fit(self,x_train,y_train):
-        assert x_train.ndim==1,\
-            "Simple Linear Regressior can only solve single feature training data"
-        assert len(x_train)==len(y_train),\
-            "the size of x_train be equal to the size of y_train"
-        x_mean=np.mean(x_train)
-        y_mean=np.mean(y_train)
-
-        num=0.0
-        d=0.0
-
-        for x,y in zip(x_train,y_train):
-            num+=(x-x_mean)*(y-y_mean)
-            d+=(x-x_mean)**2
-
-        # num=(x_train-x_mean).dot(y_train-y_mean)
-        # d=(x_train-x_mean).dot(x_train-x_mean)
-
-        self.a_=num/d
-        self.b_=y_mean-self.a_*x_mean
-        return self
-
-    def predict(self,x_predict):
-        assert x_predict.ndim==1,\
-            "Simple Linear Regressor can only solve single feature training data"
-        assert self.a_ is not None and self.b_ is not None,\
-            "must fit before predict!"
-        return np.array([self._predict(x) for x in x_predict])
-
-    def _predict(self,x_single):
-        return self.a_*x_single+self.b_
-
-    def __repr__(self):
-        return "SimpleLinearRegression2()"
-
+        return "SimpleLinearRegression()"
